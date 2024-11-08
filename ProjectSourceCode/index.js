583c426c8966116669ded4bc3eed7f1aadf4539a
Alexander Baker <Alexander.Baker-1@colorado.edu>
2024-11-08 09:20:35 -0700

    Lab 11 changes

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -164,5 +164,5 @@ app.get('/logout', (req, res) => {
 
 // -------------------------------------  START THE SERVER   ----------------------------------------------
 
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
