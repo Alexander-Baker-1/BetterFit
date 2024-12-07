3cc1f01c879e2647e2d2459f2c8143dcd41ba281
Alexander Baker <Alexander.Baker-1@colorado.edu>
2024-12-07 12:55:14 -0700

    Fix 2

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -6,11 +6,13 @@ CREATE TABLE users (
   goals VARCHAR(700)
 );
 
+DROP TABLE IF EXISTS MuscleGroup;
 CREATE TABLE MuscleGroup (
     muscle_group_id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
+DROP TABLE IF EXISTS Exercise;
 CREATE TABLE Exercise (
     exercise_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -27,6 +29,7 @@ CREATE TABLE user_exercises (
   username VARCHAR(255) NOT NULL REFERENCES users (username),
   PRIMARY KEY (exercise_id, username) -- ensures unique user-exercise combinations
 );
+
 DROP TABLE IF EXISTS FavoriteRecipe;
 CREATE TABLE FavoriteRecipe (
     recipe_id SERIAL PRIMARY KEY,
